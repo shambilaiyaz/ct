bdad3d46a5f305a04a8a7ac77df82b2ed107ba52
Shambil Aiyaz <shambilaiyaz@proton.me>
2026-04-02 03:00:54 +0600

    a path to vedio file

--- ct.js
+++ ct.js
@@ -1,7 +1,7 @@
-	function showAlert() {
+function showAlert() {
   alert("welcome to ct web");
 }
-	function playSound() {
-  let screenreader = document.getElementById('#reader').new Audio("2026-03-31 21-51-52.mkv");
+function playSound() {
+  let screenreader = new Audio("/home/sh/Videos/2026-03-31 21-51-52.mkv");
   screenreader.play();
 }
