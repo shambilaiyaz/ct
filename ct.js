7cc8f9273f03b6f2202faa4a577236a810662fed
Shambil Aiyaz <shambilaiyaz@proton.me>
2026-04-09 02:44:35 +0600

    passcode added so user knows my web

--- ct.js
+++ ct.js
@@ -2,6 +2,19 @@ function showAlert() {
   alert("welcome to ct web");
 }
 function playSound() {
-  let screenreader = new Audio("ct-video/cat-screenreader.mkv");
+  let screenreader = new Audio(
+    "https://drive.proton.me/urls/KVD236APGW#cfGjbaEPEiVy",
+  );
   screenreader.play();
+}
+function checkAccess() {
+  let paskey = prompt("hint: what are you looking for in my web");
+  if (paskey && paskey.trim() === "cat learning") {
+    document.getElementById("ful page").style.display = "block";
+  } else {
+    alert(
+      " 'error' if your looking for that, my web is useless for you! find some other web pages",
+    );
+    checkAccess();
+  }
 }
